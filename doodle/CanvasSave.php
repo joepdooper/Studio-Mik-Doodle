098ca390fbe39b8648d9a1176e672bac3a935e4b
Joep <joepdooper@hotmail.com>
2018-02-17 15:21:08 +0100

    Update CanvasSave.php

--- doodle/CanvasSave.php
+++ doodle/CanvasSave.php
@@ -1,25 +1,27 @@
 <?php
-$data = $_POST['data'];
-$data = substr($data,strpos($data,",")+1);
-$data = base64_decode($data);
-$xml = simplexml_load_file("studiomik_doodle.xml");
-$sxe = new SimpleXMLElement($xml->asXML());
-foreach ($xml as $afbeeldingen) {
-  $count = $afbeeldingen->count();
+if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+  $data = $_POST['data'];
+  $data = substr($data,strpos($data,",")+1);
+  $data = base64_decode($data);
+  $xml = simplexml_load_file("studiomik_doodle.xml");
+  $sxe = new SimpleXMLElement($xml->asXML());
+  foreach ($xml as $afbeeldingen) {
+    $count = $afbeeldingen->count();
+  }
+  $fp = $count.".png";
+  file_put_contents($fp, $data);
+  $image = imagecreatefrompng("postit.png");
+  $overlay = imagecreatefrompng($fp);
+  imagealphablending($image, true);
+  imagesavealpha($image, true);
+  imagealphablending($overlay, true);
+  imagesavealpha($overlay, true);
+  imagecopy($image, $overlay, 20, 20, 0, 0, 438, 438);
+  imagedestroy($overlay);
+  imagepng($image, $fp);
+  $itemsNode = $sxe->doodles[0];
+  $itemsNode->addChild("afbeelding", $fp);
+  $sxe->asXML("studiomik_doodle.xml");
+  echo($count);  
 }
-$fp = $count.".png";
-file_put_contents($fp, $data);
-$image = imagecreatefrompng("postit.png");
-$overlay = imagecreatefrompng($fp);
-imagealphablending($image, true);
-imagesavealpha($image, true);
-imagealphablending($overlay, true);
-imagesavealpha($overlay, true);
-imagecopy($image, $overlay, 20, 20, 0, 0, 438, 438);
-imagedestroy($overlay);
-imagepng($image, $fp);
-$itemsNode = $sxe->doodles[0];
-$itemsNode->addChild("afbeelding", $fp);
-$sxe->asXML("studiomik_doodle.xml");
-echo($count);
 ?>
